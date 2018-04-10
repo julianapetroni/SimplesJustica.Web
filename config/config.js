e01e0ae31bea5ed409da1241e37e627d3fbb79bd
Guilherme Camargo <guilherme.camargo@vidibr.com>
2018-04-10 07:57:41 -0300

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const rootPath = path.normalize(__dirname + '/..');
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'simplesjustica-web'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/simplesjustica-web-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'simplesjustica-web'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/simplesjustica-web-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'simplesjustica-web'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/simplesjustica-web-production'
+  }
+};
+
+module.exports = config[env];
