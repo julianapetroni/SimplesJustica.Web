685cc25a24fb7eee2452c7ab1ba003a71550969a
Guilherme Camargo <guisfits@hotmail.com>
2018-04-17 11:11:51 -0300

    normalize port

--- config/config.js
+++ config/config.js
@@ -3,7 +3,8 @@ const env = process.env.NODE_ENV || "development";
 
 const config = require('./config.json');
 let envConfig = config[env];
-envConfig.root = path.normalize(__dirname + "/..");;
+envConfig.root = path.normalize(__dirname + "/..");
 envConfig.app = { name : "simplesjustica-web" };
+envConfig.port = process.env.PORT || envConfig.port;
 
 module.exports = envConfig;
