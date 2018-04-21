624df3657e52bd3be701f2d1182fd6035e59550a
Guilherme Camargo <guisfits@hotmail.com>
2018-04-21 04:44:49 -0300

    navegação, listar, editar empresa

--- app/controllers/company.controller.js
+++ app/controllers/company.controller.js
@@ -2,21 +2,48 @@ const mongoose = require("mongoose");
 const Company = mongoose.model("Company");
 
 exports.list = (req, res) => {
-};
-
-exports.details = (req, res) => {
+	Company.find({}, 'nameAlias name email cnpj ie')
+		.then(data => {
+			res.render('company/list', {
+				title: "Listagem",
+				companies: data
+			});
+		})
+		.catch(err => {
+			res.render('error', {
+				message: err
+			});
+		});
 };
 
 exports.form = (req, res) => {
-	res.render('company/form');
+	res.render('company/form', {
+		title: "Cadastrar",
+		company: {}
+	});
+};
+
+exports.details = (req, res) => {
+	Company.findById(req.params.id)
+	.then(data => {
+		res.render('company/form', {
+			company: data,
+			title: "Detalhes"
+		});
+	})
+	.catch(err => {
+		res.render('error', {
+			message: err
+		});
+	});
 };
 
 exports.create = (req, res) => {
 	var company = new Company(req.body);
 	company.save()
 		.then(x => {
-			res.render('../views/index.hbs', {
-				title: "Simples Justiça"
+			res.render('index', {
+				title: "Simples Justiça",
 			});
 		})
 		.catch(err => {
@@ -28,7 +55,37 @@ exports.create = (req, res) => {
 };
 
 exports.edit = (req, res) => {
+	Company
+		.findByIdAndUpdate(req.params.id,{
+			$set: {
+				nameAlias: req.body.nameAlias,
+				name: req.body.name,
+				email: req.body.email,
+				cnpj: req.body.cnpj,
+				ie: req.body.ie
+			}
+		})
+		.then(data => {
+			res.render('index');
+		})
+		.catch(e => {
+			res.render('error', {
+				message: e
+			});
+		});
 };
 
 exports.remove = (req, res) => {
+	Company
+		.findOneAndRemove(req.params.id)
+		.then(data => {
+			res.render('index', {
+				mensagem: "Empresa deletada com sucesso."
+			});
+		})
+		.catch(e => {
+			res.render('error', {
+				message: e
+			});
+		});
 };
--- app/routes/company.route.js
+++ app/routes/company.route.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/company.controller');
 
+router.get('/', controller.list);
 router.get('/cadastrar', controller.form);
 router.post('/cadastrar', controller.create);
+router.get('/:id', controller.details);
+router.put('/:id', controller.edit);
+router.delete('/:id', controller.remove);
 
 module.exports = (app) => {
 	app.use('/empresa', router);
