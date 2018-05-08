7cd1fab147eaba09c5ab53218ca6e4f49c1f4c4f
Guilherme Camargo <guisfits@hotmail.com>
2018-05-07 23:42:41 -0300

    projeto de dados, dbcontext criado

---
+++ SimplesJustica.Data/Context/SimplesJusticaContext.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using SimplesJustica.Domain.Entities;
+
+namespace SimplesJustica.Data.Context
+{
+    public class SimplesJusticaContext : DbContext
+    {
+        public SimplesJusticaContext()
+            :base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Acusado> Acusados { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Conciliador> Conciliadores { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Reclamacao> Reclamacoes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>().Configure(x => x.HasColumnType("varchar"));
+            modelBuilder.Properties<string>().Configure(x => x.HasMaxLength(255));
+        }
+    }
+}
