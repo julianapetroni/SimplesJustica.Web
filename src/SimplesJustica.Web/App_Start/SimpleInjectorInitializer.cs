57d9007df4a5b87ef5fef66a19f65c0578a41f19
Guilherme Camargo <guisfits@hotmail.com>
2018-05-12 16:54:48 -0300

    reclamacao

---
+++ src/SimplesJustica.Web/App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,32 @@
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(SimplesJustica.Web.App_Start.SimpleInjectorInitializer), "Initialize")]
+
+namespace SimplesJustica.Web.App_Start
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using SimplesJustica.IoC;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Integration.Web.Mvc;
+    
+    public static class SimpleInjectorInitializer
+    {
+        public static void Initialize()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+            
+            IoCBootstrapper.InitializeContainer(container);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            
+            container.Verify();
+            
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+    
+    }
+}
---
+++ src/SimplesJustica.Web/Controllers/ReclamacaoController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using SimplesJustica.Application.Interfaces;
+using SimplesJustica.Application.Models;
+
+namespace SimplesJustica.Web.Controllers
+{
+    public class ReclamacaoController : Controller
+    {
+        private readonly IReclamacaoAppService app;
+
+        public ReclamacaoController(IReclamacaoAppService app)
+        {
+            this.app = app;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReclamacaoModel reclamacaoModel = await app.Get(id.Value);
+            if (reclamacaoModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reclamacaoModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ReclamacaoModel reclamacaoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                reclamacaoModel.Id = Guid.NewGuid();
+                app.Add(reclamacaoModel);
+                return RedirectToAction("Index");
+            }
+
+            return View(reclamacaoModel);
+        }
+
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReclamacaoModel reclamacaoModel = await app.Get(id.Value);
+            if (reclamacaoModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reclamacaoModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ReclamacaoModel reclamacaoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                app.Update(reclamacaoModel);
+                return RedirectToAction("Index");
+            }
+            return View(reclamacaoModel);
+        }
+
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReclamacaoModel reclamacaoModel = await app.Get(id.Value);
+            if (reclamacaoModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reclamacaoModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            ReclamacaoModel reclamacaoModel = await app.Get(id);
+            app.Delete(reclamacaoModel);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                app.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
