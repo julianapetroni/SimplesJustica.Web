b49b75ad2d282a7fa05928442f86c35c29b318ab
Guilherme Camargo <guisfits@hotmail.com>
2018-05-18 00:30:17 -0300

    organização dos arquivos de front

--- src/SimplesJustica.Web/App_Start/BundleConfig.cs
+++ src/SimplesJustica.Web/App_Start/BundleConfig.cs
@@ -7,21 +7,21 @@ namespace SimplesJustica.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Public/js/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Public/js/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Public/js/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Public/js/bootstrap.js",
+                      "~/Public/js/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Public/css/bootstrap.css",
+                      "~/Public/css/site.css"));
         }
     }
 }
